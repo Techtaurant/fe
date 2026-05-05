531ea6c95daa8f5b7bb28be32e774033739ba64f
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-06 00:07:49 +0900

    modify(config): api rewrite 제거

--- next.config.ts
+++ next.config.ts
@@ -12,24 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    const apiBaseUrl =
-      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${apiBaseUrl}/api/:path*`,
-      },
-      {
-        source: "/oauth2/:path*",
-        destination: `${apiBaseUrl}/oauth2/:path*`,
-      },
-      {
-        source: "/open-api/:path*",
-        destination: `${apiBaseUrl}/open-api/:path*`,
-      },
-    ];
-  },
 };
 
 export default withNextIntl(nextConfig);
