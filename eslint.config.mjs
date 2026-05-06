9e37019d34dc20a95bb8695e6de82ca9bf895a78
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-06 23:49:51 +0900

    chore(config): eslint 활용해 locale 자동 처리하는 @/i18n/navigation만 사용하도록 경고

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,32 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+const noRestrictedImports = {
+  files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
+  rules: {
+    "no-restricted-imports": [
+      "warn",
+      {
+        paths: [
+          {
+            name: "next/link",
+            message: "'@/i18n/navigation'의 Link를 사용하세요.",
+          },
+          {
+            name: "next/navigation",
+            importNames: ["redirect", "usePathname", "useRouter", "getPathname"],
+            message: "'@/i18n/navigation'의 내비게이션 유틸을 사용하세요.",
+          },
+        ],
+      },
+    ],
+  },
+};
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  noRestrictedImports,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
