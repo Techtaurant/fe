36f1a6fddc36879c3f0d44a4836bf39234a1912f
NARARIA03 <hyeonseong0305@gmail.com>
2026-05-09 02:09:06 +0900

    chore(config): import 정렬, prettier 충돌 방지 ESLint config 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const noRestrictedImports = {
   files: ["**/*.{js,mjs,cjs,ts,tsx,jsx}"],
@@ -28,10 +30,23 @@ const noRestrictedImports = {
   },
 };
 
+const importSortConfig = {
+  files: ["**/*.{js,jsx,ts,tsx,mjs,mts}"],
+  plugins: {
+    "simple-import-sort": simpleImportSort,
+  },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   noRestrictedImports,
+  importSortConfig,
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
